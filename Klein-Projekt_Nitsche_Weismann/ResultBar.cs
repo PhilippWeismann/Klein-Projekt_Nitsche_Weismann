3e518542e399f25db077b07fd97a77953e8bbab0
Simon Nitsche <s2110438010@fhooe.at>
2022-02-12 09:21:22 +0100

    Added Class ResultBar to display Results in Console

---
+++ Klein-Projekt_Nitsche_Weismann/ResultBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klein_Projekt_Nitsche_Weismann
+{
+    class ResultBar
+    {
+    }
+}
