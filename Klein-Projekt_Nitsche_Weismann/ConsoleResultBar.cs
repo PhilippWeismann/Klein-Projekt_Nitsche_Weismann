f488b841b99883a6807ee366bf5c3cdda3b4dd8e
Simon Nitsche <s2110438010@fhooe.at>
2022-02-12 09:25:49 +0100

    Added Members to ConsoleResultBar; Renamed ResultBar class to ConoleResultBar

---
+++ Klein-Projekt_Nitsche_Weismann/ConsoleResultBar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klein_Projekt_Nitsche_Weismann
+{
+    static class ConsoleResultBar
+    {
+        #region Members
+        static double _min;
+        static double _max;
+        static double _result;
+        static int _length;
+        #endregion
+
+        //No Konstruktor at static classes
+
+        #region Methods
+
+        static void DisplayProgessBarInConsole()
+        { 
+        
+        }
+
+        #endregion
+
+
+
+
+    }
+}
